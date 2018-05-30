590ab6bfe6aa171a50d8453159934544c85d99b3
Avrumi Friedman <alex+6@lakewoodprogramming.com>
2018-05-29 22:27:15 -0400

    Initial commit

---
+++ CandidateTracker.Web/LayoutPageActionFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CandidateTracker.Data;
+
+namespace CandidateTracker.Web
+{
+    public class LayoutPageActionFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var manager = new CandidateRepository(Properties.Settings.Default.ConStr);
+            CandidateCounts counts = manager.GetCounts();
+            filterContext.Controller.ViewBag.CandidateCounts = counts;
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
